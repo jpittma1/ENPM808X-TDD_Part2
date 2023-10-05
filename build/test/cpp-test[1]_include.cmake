83807db0d731bd480c1ff045ef7f6961916a48e6
Rashmi Kapu <bhatrashmi.1105@gmail.com>
2023-10-05 02:35:18 -0400

    Adding tests

--- build/test/cpp-test[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/home/rashmikapu/cpp-boilerplate-v2/build/test/cpp-test[1]_tests.cmake")
-  include("/home/rashmikapu/cpp-boilerplate-v2/build/test/cpp-test[1]_tests.cmake")
-else()
-  add_test(cpp-test_NOT_BUILT cpp-test_NOT_BUILT)
-endif()
