1d3ece615e55d59d1f0958d211c122e3eb986cad
Jerry Pittman <jpittma1@umd.edu>
2023-10-09 19:12:45 -0400

    removed build files

--- build/libs/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rashmikapu/cpp-boilerplate-v2/libs
-# Build directory: /home/rashmikapu/cpp-boilerplate-v2/build/libs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("lib1")
-subdirs("lib2")
