b53889d06b41e5a166f530510c256eb530aad960
Achuthankrishna <74654704+Achuthankrishna@users.noreply.github.com>
2023-10-10 01:43:36 -0400

    Update pid.hpp

--- include/pid.hpp
+++ include/pid.hpp
@@ -42,6 +42,6 @@ class PID_controller{
    * @return double : Generated output velocity
    */
   double compute(double vel);
-  bool check(double vel);
+  bool check(double vel,double Kp,double Ki,double Kd,double initial_vel);
 
 };
